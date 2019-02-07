a60e563764b5b1ab3984132bc8727644467ecdf3
Peter-Sten Annus <petersten.annus@gmail.com>
2019-02-07 14:31:34 +0200

    Added IntelliJ conf files and reformatted most files

---
+++ server/src/main/java/com/hrp/springapp/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.hrp.springapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface TodoRepository extends JpaRepository<Todo, Long> {
+}
