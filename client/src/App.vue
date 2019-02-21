6a4027d28f2106abc1dbf29f6be44d94e34aa18a
hekruu <hekruu@ttu.ee>
2019-02-21 09:02:35 +0200

    NavBar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
-    <Todos />
-    <footer class="info">
-      <p>Based on a project written by <a href="http://evanyou.me">Evan You</a></p>
-      <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
-      <p>Modified for this tutorial by Andrew Hughes</p>
-    </footer>
+    <Homepage />
   </div>
 </template>
 
 <script>
-  import Todos from './components/Todos'
+  import Homepage from "./components/Navbar";
   // app Vue instance
   const app = {
     name: 'app',
     components: {
-      Todos
+        Homepage
     },
     // app initial state
     data: () => {
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="nav dark-nav">
+        <div class="container">
+            <div class="nav-heading">
+                <a class="brand" href="#">
+                    <img src="logo.png"/>
+                </a>
+            </div>
+            <div class="menu" id="open-navbar1">
+                <ul class="list">
+                    <li><a href="#">page 1</a></li>
+                    <li><a href="#">page 2</a></li>
+                    <li><a href="#">page 3</a></li>
+                    <li><a href="#">page 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
