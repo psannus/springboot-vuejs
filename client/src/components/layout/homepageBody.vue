fa87c347ab2aa48d713bed5832761ac52140f590
hekruu <hekruu@ttu.ee>
2019-05-08 23:45:12 +0300

    Information about expired and expiring products

--- client/src/components/layout/homepageBody.vue
+++ client/src/components/layout/homepageBody.vue
@@ -31,26 +31,25 @@
         </div>
 
         <div class="container">
-            <div class="best">
-                <h1>Expiration Date</h1>
+            <div class="best pb-3">
+                <h1>Expiring products</h1>
             </div>
-            <div class="row mb-5">
-                <div class="col-sm-4">
-                    <img src="https://s3.amazonaws.com/tt-trackify/assets/milk.png" alt="picture">
-                </div>
-                <div class="col-sm-4" style="align-content: center">
-                    <img src="https://s3.amazonaws.com/tt-trackify/assets/liha.png" alt="picture">
-                </div>
-                <div class="col-sm-4">
-                    <img src="https://s3.amazonaws.com/tt-trackify/assets/vorst.png" alt="picture">
-                </div>
+            <div>
+                <b-table v-if="expiringProducts.length !== 0" hover :items="expiringProducts"
+                         :fields="fields"></b-table>
+                <b-table v-if="expired.length !== 0" style="color:red" hover :items="expired"
+                         :fields="fields"></b-table>
             </div>
+
+            <div v-if="show_table_boolean">
+                <h3 style="text-align: center" class="p-4">No expiring products right now!</h3>
+            </div>
+
         </div>
         <div class="jumbotron">
             <div class="container" style="text-align: center">
                 <h4 style="font-family: Gotham">
                     Life expectancy would grow by leaps and bounds if green vegetables smelled as good as bacon -
-
                     Doug Larson
                 </h4>
             </div>
@@ -72,9 +71,71 @@
 </template>
 
 <script>
+    import axios from "axios/index"
+    import api from "../../Api.js";
+
+    axios.defaults.withCredentials = true;
+
+
     export default {
-        name: "homepageBody"
+        name: "homepageBody",
+
+        data() {
+            return {
+                fields: ['expiryDate', 'name', 'amount', 'price'],
+                stock: [],
+                currYear: "",
+                currMonth: "",
+                currDay: "",
+                expiringProducts: [],
+                expired: []
+            }
+        },
+
+        computed: {
+            show_table_boolean() {
+                return (this.expiringProducts.length === 0) && (this.expired.length === 0)
+            }
+        },
+
+        mounted() {
+            axios.get(api.url + '/basket-list', {
+                params: {
+                    id: 1
+                }
+            })
+                .then(res => {
+                    res.data.forEach(i => {
+                        this.stock = this.stock.concat(i.productList);
+                    });
+                    this.getExpiringProducts();
+                });
+        },
+
+        methods: {
+            getExpiringProducts() {
+                let expiring = [];
+                let expired = [];
+
+                this.stock.forEach(function (item) {
+                    if (item.expiryDate !== "") {
+                        if (Date.parse(item.expiryDate) >= (new Date().getTime()) &&
+                            Date.parse(item.expiryDate) <= (new Date().getTime() + 259200000)) {
+                            expiring.push(item);
+                        }
+                        if (Date.parse(item.expiryDate) < (new Date().getTime())) {
+                            expired.push(item);
+
+                        }
+                    }
+                });
+                this.expiringProducts = expiring;
+                this.expired = expired;
+            }
+        }
     }
+
+
 </script>
 
 <style scoped>
